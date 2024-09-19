f835cf3ef825551e0c9492828dd4c4f161dec45c
WhatupJayy <jaiy07.2002@gmail.com>
2024-09-20 03:05:46 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import { RiFacebookBoxFill, RiInstagramFill, RiTwitterFill } from '@remixicon/react'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='max-w-7xl mx-auto'>
+        <div className='flex flex-col md:flex-row justify-between items-center border-t-2 py-4'>
+            <div className='flex space-x-6 mb-2'>
+                <a href="http://www.facebook.com" target='_blank' rel="noopener noreferrer" aria-label='Visit our Facebook page'>
+                    <RiFacebookBoxFill />
+                </a>
+                <a href="http://www.twitter.com" target='_blank' rel="noopener noreferrer" aria-label='Visit our Twitter page'>
+                    <RiTwitterFill />
+                </a>
+                <a href="http://www.instagram.com" target='_blank' rel="noopener noreferrer" aria-label='Visit our Instagram page'>
+                    <RiInstagramFill />
+                </a>
+            </div>
+            <p className='text-sm'>&copy; 2024 VastuSpaze. All rights reserved.</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
